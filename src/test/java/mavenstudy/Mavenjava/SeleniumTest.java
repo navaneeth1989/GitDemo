8550b5868483dffa253cdddc2a478353a7e157f5
navaneeth1989 <navaneethpayyoli@gmail.com>
2020-08-13 13:41:54 +0530

    Architect Chagnes will make here

--- src/test/java/mavenstudy/Mavenjava/SeleniumTest.java
+++ src/test/java/mavenstudy/Mavenjava/SeleniumTest.java
@@ -12,4 +12,13 @@ public class SeleniumTest {
 	public void elementUI(){
 		System.out.println("elementUI");
 	}
+	
+	@Test
+	public void elementUI1(){
+		System.out.println("fot git study_adding to brach with these changes");
+	}
+	@Test
+	public void elementUI2(){
+		System.out.println("fot git study_adding to brach with these changes");
+	}
 }
